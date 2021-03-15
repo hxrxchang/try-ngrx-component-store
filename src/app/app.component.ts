be631ed9bc99fd784acf4a1b9078b45f27ea0118
hxrxchang <mail@hxrxchang.dev>
2021-03-15 11:42:56 +0900

    updaterで複数property更新を試した

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie, MoviesStore } from './movies.store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MoviesStore],
 })
-export class AppComponent {
-  title = 'try-ngrx-component-store';
+export class AppComponent implements OnInit {
+  movies$ = this.moviesStore.movies$;
+
+  constructor(private readonly moviesStore: MoviesStore) {}
+
+  ngOnInit(): void {
+    this.moviesStore.state$.subscribe((s) => console.log(s));
+  }
+
+  add(movieName: string, author: string): void {
+    this.moviesStore.add({ movie: { name: movieName }, author });
+    // this.moviesStore.addMovie({ name: movieName });
+    // this.moviesStore.addAuthor(author);
+  }
 }
--- src/app/movies.store.ts
+++ src/app/movies.store.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
 
 export interface Movie {
   name: string;
@@ -7,11 +8,37 @@ export interface Movie {
 
 export interface MoviesState {
   movies: Movie[];
+  authors: string[];
 }
+
+const initialState: MoviesState = {
+  movies: [{ name: 'Titanic' }],
+  authors: [],
+};
 
 @Injectable()
 export class MoviesStore extends ComponentStore<MoviesState> {
   constructor() {
-    super({ movies: [] });
+    super(initialState);
   }
+
+  readonly movies$: Observable<Movie[]> = this.select((state) => state.movies);
+
+  readonly add = this.updater(
+    (state, params: { movie: Movie; author: string }) => ({
+      ...state,
+      movies: [...state.movies, params.movie],
+      authors: [...state.authors, params.author],
+    })
+  );
+
+  readonly addMovie = this.updater((state, movie: Movie) => ({
+    ...state,
+    movies: [...state.movies, movie],
+  }));
+
+  readonly addAuthor = this.updater((state, author: string) => ({
+    ...state,
+    authors: [...state.authors, author],
+  }));
 }
